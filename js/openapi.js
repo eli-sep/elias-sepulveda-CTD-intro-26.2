ac40d27e01e84e050f71f847ae87b42254d1822b
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-06-06 19:21:27 -0700

    Initial branch commit to link hosted back end.

---
+++ js/openapi.js
@@ -0,0 +1,12 @@
+const API_KEY = "fbebff44ba811d5fbb0594fad0bc9cb50ee12b60";
+
+async function getSuperHero() {
+  const response = await fetch(`https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/characters/?api_key=${API_KEY}&format=json`);
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+}
+
+getSuperHero();
+
+console.log("test")
