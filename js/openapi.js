115e05e5fea8e041968bf50bbf08ff75d5aa7f77
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-06-06 20:27:35 -0700

    Testing fetch w/o cors anywhere.

--- js/openapi.js
+++ js/openapi.js
@@ -1,7 +1,7 @@
 const API_KEY = "fbebff44ba811d5fbb0594fad0bc9cb50ee12b60";
 
 async function getSuperHero() {
-  const response = await fetch(`https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/characters/?api_key=${API_KEY}&format=json`);
+  const response = await fetch(`https://comicvine.gamespot.com/api/characters/?api_key=${API_KEY}&format=json`);
   console.log(response);
   const data = await response.json();
   console.log(data);
@@ -9,4 +9,4 @@ async function getSuperHero() {
 
 getSuperHero();
 
-console.log("test")
+console.log("test");
