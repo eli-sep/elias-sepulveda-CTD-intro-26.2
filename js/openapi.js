a5cea5c15d7a508a862de3a5d00640723bf91bc1
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-06-13 09:56:24 -0700

    Updated website layout 1 character card. Added custom fonts and styling. Need to redo api call and verify data.

--- js/openapi.js
+++ js/openapi.js
@@ -1,12 +1,89 @@
-const API_KEY = "fbebff44ba811d5fbb0594fad0bc9cb50ee12b60";
 
-async function getSuperHero() {
-  const response = await fetch(`https://comicvine.gamespot.com/api/characters/?api_key=${API_KEY}&format=json`);
-  console.log(response);
+
+async function getMarvelCharacters() {
+  const response = await fetch(`${apiBaseURL}/api/marvel`);
   const data = await response.json();
-  console.log(data);
+
+  const marvelSelChars = data.filter((object) => marvelSelName.includes(object.name) && object.id < 15000);
+  const marvelCharApi = marvelSelChars.map(({ name, api_detail_url }) => ({ name, api_detail_url }));
+  console.log(marvelCharApi);
+
+  for (const character of marvelCharApi) {
+    const response = await fetch(`${apiBaseURL}/api/marvel-character`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(character),
+    });
+    const charData = await response.json();
+    marvelChars[charData.name] = charData;
+  }
+  console.log(marvelChars);
+  console.log("This is the end!");
 }
 
-getSuperHero();
+async () => {
+  await getMarvelCharacters();
+};
 
+getMarvelCharacters()
+
+// async function getSpiderMan() {
+//   const response = await fetch("https://comicvine.gamespot.com/api/character/4005-1440/")
+//   const data = await response.json()
+  
+// }
+
+
+
+/*
+
+(16) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+0
+: 
+{name: 'Wolverine', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1440/'}
+1
+: 
+{name: 'Captain America', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1442/'}
+2
+: 
+{name: 'Spider-Man', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1443/'}
+3
+: 
+{name: 'Iron Man', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1455/'}
+4
+: 
+{name: 'Scarlet Witch', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1466/'}
+5
+: 
+{name: 'Black Panther', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1477/'}
+6
+: 
+{name: 'Iron Fist', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1492/'}
+7
+: 
+{name: 'Hulk', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-2267/'}
+8
+: 
+{name: 'Thor', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-2268/'}
+9
+: 
+{name: 'Black Widow', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-3200/'}
+10
+: 
+{name: 'Star-Lord', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-10957/'}
+11
+: 
+{name: 'Wolverine', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-76835/'}
+{name: 'Wolverine', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-86165/'}
+{name: 'Thor', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-97484/'}
+{name: 'Wolverine', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-181781/'}: 
+{name: 'Iron Man', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-196208/'}
+
+
+*/
+
+
+// console.log(marvelChars)
 console.log("test");
