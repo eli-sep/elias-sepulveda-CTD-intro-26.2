b07e39aba893866393bbb88f12697e91e151d82b
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-06-14 00:01:15 -0700

    API website function and styled.  Working on testing error handling.

--- js/openapi.js
+++ js/openapi.js
@@ -2,7 +2,8 @@ const comicVineKey = "fbebff44ba811d5fbb0594fad0bc9cb50ee12b60";
 const corsURL = "https://cors-anywhere.herokuapp.com/";
 const corURLdemo = "https://cors-anywhere.herokuapp.com/corsdemo";
 
-const spiderManURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1443/`;
+
+const spiderManURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1443a/`;
 const wolverineURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1440/`;
 const captainAmericaURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1442/`;
 const ironManURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1455/`;
@@ -14,7 +15,12 @@ const thorURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-226
 const blackWidowURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-3200/`;
 const starLordURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-10957/`;
 
-function domRender(data) {
+const batmanURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1699/`
+const wonderWomanURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-2048/`
+const theFlashURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-22804/`
+const supermanURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1807/`
+
+function domRender(data, style) {
   //data extraction
   const name = data.name.toUpperCase();
   const realName = data.real_name;
@@ -24,16 +30,17 @@ function domRender(data) {
   const image = data.image.medium_url;
 
   //document data
-  const charName = document.querySelector("#marvel-title");
+  const charName = document.querySelector(".char-name");
   const charImage = document.querySelector(".char-image");
-  const charRealName = document.querySelector(".char-name");
+  const charRealName = document.querySelector(".char-real-name");
   const charBirth = document.querySelector(".char-birth");
   const charBio = document.querySelector(".char-bio");
   const charPowers = document.querySelector(".char-powers");
 
   //DOM maniplation
-  charImage.src = image;
+  charImage.src = image;;
   charName.innerText = name;
+  charName.id = style;
   charRealName.innerHTML = `<strong>Real Name: </strong>${realName}`;
   charBirth.innerHTML = `<strong>Birthdate: </strong>${birthdate}`;
   charBio.innerHTML = `<strong>Bio: </strong>${bio}`;
@@ -43,34 +50,97 @@ function domRender(data) {
 
 async function getSpiderMan() {
   //api data
-  const response = await fetch(`${spiderManURL}?api_key=${comicVineKey}&format=json`);
-  const object = await response.json();
-  const data = object.results;
-  domRender(data);
+  try {
+    const response = await fetch(`${spiderManURL}?api_key=${comicVineKey}&format=json`);
+    console.log("Status", response.status)
+    console.log("Response", response)
+    if(!response.ok){
+      throw new Error(`HTTP`)
+    }
+    const object = await response.json();
+    const data = object.results;
+    const titleStyle = "marvel-title";
+    console.log("Object", object)
+    console.log("Data", data)
+    domRender(data, titleStyle);
+  } catch (error) {
+    console.log("This is the log", error + status)
+    console.error("This is the error", error)
+  }
 }
 
 async function getBlackWidow() {
   //api data
-  const response = await fetch(`${blackWidowURL}?api_key=${comicVineKey}&format=json`);
-  const object = await response.json();
-  const data = object.results;
-  domRender(data);
+  try{
+    const response = await fetch(`${blackWidowURL}?api_key=${comicVineKey}&format=json`);
+    console.log("Status", response.status)
+    console.log("Response", response)
+    if(!response.ok){
+      throw new Error(`HTTP`)
+    }
+    const object = await response.json();
+    const data = object.results;
+    const titleStyle = "marvel-title";
+    domRender(data, titleStyle);
+  } catch (error) {
+    console.log("This is the log", error + status)
+    console.error("This is the error", error)
+  }
 }
+    
 
 async function getStarLord() {
   //api data
   const response = await fetch(`${starLordURL}?api_key=${comicVineKey}&format=json`);
   const object = await response.json();
   const data = object.results;
-  domRender(data);
+  const titleStyle = "marvel-title";
+  domRender(data, titleStyle);
+}
+
+async function getBatman() {
+  //api data
+  const response = await fetch(`${batmanURL}?api_key=${comicVineKey}&format=json`);
+  const object = await response.json();
+  const data = object.results;
+  const titleStyle = "dc-title";
+  domRender(data, titleStyle);
+}
+
+async function getWonderWoman() {
+  //api data
+  const response = await fetch(`${wonderWomanURL}?api_key=${comicVineKey}&format=json`);
+  const object = await response.json();
+  const data = object.results;
+  const titleStyle = "dc-title";
+  domRender(data, titleStyle);
+}
+
+async function getTheFlash() {
+  //api data
+  const response = await fetch(`${theFlashURL}?api_key=${comicVineKey}&format=json`);
+  const object = await response.json();
+  const data = object.results;
+  const titleStyle = "dc-title";
+  domRender(data, titleStyle);
+}
+
+async function getSuperman() {
+  //api data
+  const response = await fetch(`${supermanURL}?api_key=${comicVineKey}&format=json`);
+  const object = await response.json();
+  const data = object.results;
+  const titleStyle = "dc-title";
+  domRender(data, titleStyle);
 }
 
 const spiderMan = document.querySelector("#spider-man");
 const blackWidow = document.querySelector("#black-widow");
 const starLord = document.querySelector("#star-lord");
-const batman = document.querySelector("#spider-man");
-const wonderWoman = document.querySelector("#spider-man");
-const theFlash = document.querySelector("#spider-man");
+const batman = document.querySelector("#batman");
+const wonderWoman = document.querySelector("#wonder-woman");
+const theFlash = document.querySelector("#the-flash");
+const superman = document.querySelector("#superman");
 
 spiderMan.addEventListener("click", (event) => {
   getSpiderMan();
@@ -82,91 +152,12 @@ starLord.addEventListener("click", (event) => {
   getStarLord();
 });
 
-// getSpiderMan();
-
-// const apiBaseURL = window.location.hostname === "127.0.0.1" ? "http://localhost:3000" : "https://api-backend-dw3n.onrender.com";
-// const marvelSelName = ["Spider-Man", "Wolverine", "Captain America", "Iron Man", "Black Widow", "Iron Fist", "Scarlet Witch", "Black Panther", "Hulk", "Thor", "Star-Lord", "Logan"];
-// const marvelChars = {};
-
-// async function getMarvelCharacters() {
-//   const response = await fetch(`${apiBaseURL}/api/marvel`);
-//   const data = await response.json();
-
-//   const marvelSelChars = data.filter((object) => marvelSelName.includes(object.name) && object.id < 15000);
-//   const marvelCharApi = marvelSelChars.map(({ name, api_detail_url }) => ({ name, api_detail_url }));
-//   console.log(marvelCharApi);
-
-//   for (const character of marvelCharApi) {
-//     const response = await fetch(`${apiBaseURL}/api/marvel-character`, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(character),
-//     });
-//     const charData = await response.json();
-//     marvelChars[charData.name] = charData;
-//   }
-//   console.log(marvelChars);
-//   console.log("This is the end!");
-// }
-
-// async () => {
-//   await getMarvelCharacters();
-// };
-
-// getMarvelCharacters()
-
-// async function getSpiderMan() {
-//   const response = await fetch("https://comicvine.gamespot.com/api/character/4005-1440/")
-//   const data = await response.json()
-
-// }
-
-/*
-{name: 'Spider-Man', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1443/'}
-{name: 'Wolverine', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1440/'}
-{name: 'Captain America', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1442/'}
-{name: 'Iron Man', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1455/'}
-{name: 'Scarlet Witch', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1466/'}
-{name: 'Black Panther', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1477/'}
-{name: 'Iron Fist', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-1492/'}
-{name: 'Hulk', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-2267/'}
-{name: 'Thor', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-2268/'}
-{name: 'Black Widow', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-3200/'}
-{name: 'Star-Lord', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-10957/'}
-
-(16) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
-0
-: 
-1
-: 
-: 
-2
-3
-: 
-4
-: 
-5
-: 
-6
-: 
-8
-7
-: 
-: 
-9
-: 
-10
-: 
-11
-: 
-{name: 'Wolverine', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-76835/'}
-{name: 'Wolverine', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-86165/'}
-{name: 'Thor', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-97484/'}
-{name: 'Wolverine', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-181781/'}: 
-{name: 'Iron Man', api_detail_url: 'https://comicvine.gamespot.com/api/character/4005-196208/'}
-
-
-*/
-// console.log(marvelChars)
+batman.addEventListener("click", (event) => {
+  getBatman();
+});
+wonderWoman.addEventListener("click", (event) => {
+  getWonderWoman();
+});
+superman.addEventListener("click", (event) => {
+  getSuperman();
+});
