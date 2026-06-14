93e9c9b272321c8437e647c063a40ed8afc3dca9
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-06-14 15:02:00 -0700

    Commented out code to test refactoring. One async function then passed parameters to it.

--- js/openapi.js
+++ js/openapi.js
@@ -1,8 +1,9 @@
-const comicVineKey = "fbebff44ba811d5fbb0594fad0bc9cb50ee12b60";
+// const comicVineKey = "fbebff44ba811d5fbb0594fad0bc9cb50ee12b60";
+const comicVineKey = "d1bd5d987b0de3de1767337fd9dc6571c3b326be";
 const corsURL = "https://cors-anywhere.herokuapp.com/";
 const corURLdemo = "https://cors-anywhere.herokuapp.com/corsdemo";
 
-const spiderManURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1443a/`;
+const spiderManURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1443/`;
 const wolverineURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1440/`;
 const captainAmericaURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1442/`;
 const ironManURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1455/`;
@@ -12,21 +13,21 @@ const ironFistURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005
 const hulkURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-2267/`;
 const thorURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-2268/`;
 const blackWidowURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-3200/`;
-const starLordURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-10957KJHGa/`;
-
+const starLordURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-10957/`;
 const batmanURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1699/`;
 const wonderWomanURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-2048/`;
 const theFlashURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-22804/`;
 const supermanURL = `${corsURL}https://comicvine.gamespot.com/api/character/4005-1807/`;
 
-function errorRender() {}
-
-function domRender(data, style, corsBlocked=false) {
+function domRender(data, style, err = 0) {
   //data extraction
-  if(corsBlocked){
-     const title = document.querySelector(".char-name");
-     charName.innerHTML = '<a href="index.html">Click here to enable CORS.</a>';
-  }
+  if (err === 1) {
+    const title = document.querySelector(".char-name");
+    title.innerHTML = `<a href=${corURLdemo}>Click here to enable CORS.</a>`;
+  } else if (err === 2) {
+    const title = document.querySelector(".char-name");
+    title.innerHTML = `Something went wrong please try again later.`;
+  } else {
     const name = data.name.toUpperCase();
     const realName = data.real_name;
     const birthdate = data.birth;
@@ -51,135 +52,174 @@ function domRender(data, style, corsBlocked=false) {
     charBio.innerHTML = `<strong>Bio: </strong>${bio}`;
     const power = powers.map((power) => power.name);
     charPowers.innerHTML = `<strong>Powers: </strong>${power.join(", ")}`;
+  }
 }
 
-async function getSpiderMan() {
+// async function getSpiderMan() {
+//   //api data
+//   try {
+//     const response = await fetch(`${spiderManURL}?api_key=${comicVineKey}&format=json`);
+//     if (!response.ok) {
+//       const errorMessage = await response.text();
+//       if (errorMessage.includes("corsdemo")) {
+//         throw new Error(`Heroku CORS not enabled.  Click Link to enable CORS. ${corURLdemo}`, { cause: "CORS" });
+//       } else {
+//         throw new Error(`${response.status} - ${response.statusText}. Something went wrong please try again later.`);
+//         const object = await response.text();
+//         const data = object.results;
+//         const titleStyle = "marvel-title";
+//         domRender(data, titleStyle);
+//       }
+//     }
+//   } catch (error) {
+//     if (error.cause === "CORS") {
+//       console.error(`${error}`);
+//       domRender(null, null, 1);
+//     } else {
+//       console.error(`${error}`);
+//       domRender(null, null, 2);
+//     }
+//   }
+// }
+
+// async function getBlackWidow() {
+//   //api data
+//   try {
+//     const response = await fetch(`${blackWidowURL}?api_key=${comicVineKey}&format=json`);
+//     if (!response.ok) {
+//       const errorMessage = await response.text();
+//       if (errorMessage.includes("corsdemo")) {
+//         throw new Error(`Heroku CORS not enabled.  Click Link to enable CORS. ${corURLdemo}`, { cause: "CORS" });
+//       } else {
+//         throw new Error(`${response.status} - ${response.statusText}. Something went wrong please try again later.`);
+//       }
+//       const object = await response.json();
+//       const data = object.results;
+//       const titleStyle = "marvel-title";
+//       domRender(data, titleStyle);
+//     }
+//   } catch (error) {
+//     if (error.cause === "CORS") {
+//       console.error(`${error}`);
+//       domRender(null, null, 1);
+//     } else {
+//       console.error(`${error}`);
+//       domRender(null, null, 2);
+//     }
+//   }
+// }
+
+// async function getStarLord() {
+//   //api data
+//   try {
+//     const response = await fetch(`${starLordURL}?api_key=${comicVineKey}&format=json`);
+//     isOk = response.ok;
+//     status = response.status;
+//     statusText = response.statusText;
+//     console.log("Status", response.status);
+//     console.log("Response", response);
+//     console.log("Objecijlkjt");
+
+//     if (!response.ok) {
+//       const errorMessage = await response.text();
+//       if (errorMessage.includes("corsdemo")) {
+//         console.log("Cors Error", errorMessage);
+//         throw new Error("Please visit");
+//       }
+//       console.log("THIS IS THE ERROR MESSAGE", errorMessage);
+//       throw new Error();
+//     }
+//     // console.log("Error before catch", error);
+//     // console.log("Error before catch", error);
+//     // console.log("Objecijlkjt", object1);
+//     // const object = await response.json();
+//     // const data = object.results;
+//     // const titleStyle = "marvel-title";
+//     // domRender(data, titleStyle);
+//   } catch (error) {
+//     console.log("Error", Object.getOwnPropertyNames(error));
+//     console.log("Error", error);
+//     console.log("Error", error.stack);
+//     console.log("Error", error.name);
+//     console.log("Error", typeof error.message);
+//     if (error.message.includes("/corsdemo")) {
+//     }
+//   }
+//   // try {
+//   //   if (!isOk) {
+//   //     throw new Error(`HTTP`);
+//   //   }
+//   // } catch (error) {
+//   //   console.log(error);
+//   // }
+// }
+
+// async function getBatman() {
+//   //api data
+//   try {
+//     const response = await fetch(`${batmanURL}?api_key=${comicVineKey}&format=json`);
+//     // if(respon)
+//     const object = await response.json();
+//     const data = object.results;
+//     const titleStyle = "dc-title";
+//     domRender(data, titleStyle);
+//   } catch (error) {
+//     domRender();
+//   }
+// }
+
+// async function getWonderWoman() {
+//   //api data
+//   const response = await fetch(`${wonderWomanURL}?api_key=${comicVineKey}&format=json`);
+//   const object = await response.json();
+//   const data = object.results;
+//   const titleStyle = "dc-title";
+//   domRender(data, titleStyle);
+// }
+
+// async function getTheFlash() {
+//   //api data
+//   const response = await fetch(`${theFlashURL}?api_key=${comicVineKey}&format=json`);
+//   const object = await response.json();
+//   const data = object.results;
+//   const titleStyle = "dc-title";
+//   domRender(data, titleStyle);
+// }
+
+// async function getSuperman() {
+//   //api data
+//   const response = await fetch(`${supermanURL}?api_key=${comicVineKey}&format=json`);
+//   const object = await response.json();
+//   const data = object.results;
+//   const titleStyle = "dc-title";
+//   domRender(data, titleStyle);
+// }
+
+async function getCharacter(url, style) {
   //api data
   try {
-    const response = await fetch(`${spiderManURL}?api_key=${comicVineKey}&format=json`);
-    console.log("Status", response.status);
-    console.log("Response", response);
+    const response = await fetch(`${url}?api_key=${comicVineKey}&format=json`);
     if (!response.ok) {
-      throw new Error(`HTTP`);
-    }
-    const object = await response.text();
-    const data = object.results;
-    const titleStyle = "marvel-title";
-    console.log("Object", object);
-    console.log("Data", data);
-    domRender(data, titleStyle);
-  } catch (error) {
-    console.log("This is the log", error + status);
-    console.error("This is the error", error);
-  }
-}
-
-async function getBlackWidow() {
-  //api data
-  try {
-    const response = await fetch(`${blackWidowURL}?api_key=${comicVineKey}&format=json`);
-    console.log("Status", response.status);
-    console.log("Response", response);
-    const object = await response.json();
-    const data = object.results;
-    const titleStyle = "marvel-title";
-    domRender(data, titleStyle);
-  } catch (error) {
-    console.log("This is the log", error + status);
-    console.error("This is the error", error);
-  }
-  // if(!response.ok){
-  //   throw new Error(`HTTP`)
-  // }
-}
-
-async function getStarLord() {
-  //api data
-  let isOk;
-  let status;
-  let statusText;
-
-  try {
-    const response = await fetch(`${starLordURL}?api_key=${comicVineKey}&format=json`);
-    isOk = response.ok;
-    status = response.status;
-    statusText = response.statusText;
-    console.log("Status", response.status);
-    console.log("Response", response);
-    console.log("Objecijlkjt");
-
-    if(!response.ok){
       const errorMessage = await response.text();
-      if(errorMessage.includes("corsdemo")){
-        console.log("Cors Error", errorMessage)
-        throw new Error("Please visit")
+      if (errorMessage.includes("corsdemo")) {
+        throw new Error(`Heroku CORS not enabled.  Click Link to enable CORS. ${corURLdemo}`, { cause: "CORS" });
+      } else {
+        throw new Error(`${response.status} - ${response.statusText}. Something went wrong please try again later.`);
       }
-      console.log("THIS IS THE ERROR MESSAGE", errorMessage)
-      throw new Error();
     }
-    // console.log("Error before catch", error);
-    // console.log("Error before catch", error);
-    // console.log("Objecijlkjt", object1);
-    // const object = await response.json();
-    // const data = object.results;
-    // const titleStyle = "marvel-title";
-    // domRender(data, titleStyle);
-  } catch (error) {
-    console.log("Error", Object.getOwnPropertyNames(error));
-    console.log("Error", error);
-    console.log("Error", error.stack);
-    console.log("Error", error.name);
-    console.log("Error", typeof error.message);
-    if (error.message.includes("/corsdemo")) {
-    }
-  }
-  // try {
-  //   if (!isOk) {
-  //     throw new Error(`HTTP`);
-  //   }
-  // } catch (error) {
-  //   console.log(error);
-  // }
-}
-
-async function getBatman() {
-  //api data
-  try{
-    const response = await fetch(`${batmanURL}?api_key=${comicVineKey}&format=json`);
     const object = await response.json();
     const data = object.results;
-    const titleStyle = "dc-title";
+    const titleStyle = style;
     domRender(data, titleStyle);
   } catch (error) {
-    domRender()
+    if (error.cause === "CORS") {
+      console.error(`${error}`);
+      domRender(null, null, 1);
+    } else {
+      console.error(`${error}`);
+      domRender(null, null, 2);
+    }
   }
-}
-
-async function getWonderWoman() {
-  //api data
-  const response = await fetch(`${wonderWomanURL}?api_key=${comicVineKey}&format=json`);
-  const object = await response.json();
-  const data = object.results;
-  const titleStyle = "dc-title";
-  domRender(data, titleStyle);
-}
-
-async function getTheFlash() {
-  //api data
-  const response = await fetch(`${theFlashURL}?api_key=${comicVineKey}&format=json`);
-  const object = await response.json();
-  const data = object.results;
-  const titleStyle = "dc-title";
-  domRender(data, titleStyle);
-}
-
-async function getSuperman() {
-  //api data
-  const response = await fetch(`${supermanURL}?api_key=${comicVineKey}&format=json`);
-  const object = await response.json();
-  const data = object.results;
-  const titleStyle = "dc-title";
-  domRender(data, titleStyle);
 }
 
 const spiderMan = document.querySelector("#spider-man");
@@ -191,21 +231,21 @@ const theFlash = document.querySelector("#the-flash");
 const superman = document.querySelector("#superman");
 
 spiderMan.addEventListener("click", (event) => {
-  getSpiderMan();
+  getCharacter(spiderManURL, "marvel-title");
 });
 blackWidow.addEventListener("click", (event) => {
-  getBlackWidow();
+  getCharacter(blackWidowURL, "marvel-title");
 });
 starLord.addEventListener("click", (event) => {
-  getStarLord();
+  getCharacter(starLordURL, "marvel-title");
 });
 
 batman.addEventListener("click", (event) => {
-  getBatman();
+  getCharacter(batmanURL, "dc-title");
 });
 wonderWoman.addEventListener("click", (event) => {
-  getWonderWoman();
+  getCharacter(wonderWomanURL, "dc-title");
 });
 superman.addEventListener("click", (event) => {
-  getSuperman();
+  getCharacter(supermanURL, "dc-title");
 });
